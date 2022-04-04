c5566bb2686f3b858d6fc4bd6c1e321d79a01d11
apisu <aurelien.pisu@cohorte-technologies.com>
2022-04-04 16:36:46 +0200

    test 2 item with inheritance

--- model/test.py
+++ model/test.py
@@ -2,7 +2,7 @@ from ycappuccino.core.model.decorators import Item, Property
 from ycappuccino.core.model.model import Model
 
 
-@Item(collection="tests",name="test")
+@Item(collection="tests",name="test", plural="tests")
 class Test(Model):
     def __init__(self, a_dict):
         super().__init__(a_dict)
@@ -10,4 +10,15 @@ class Test(Model):
 
     @Property(name="name")
     def name(self, a_value):
+        self._name = a_value
+
+
+@Item(collection="tests",name="test2",plural="tests2")
+class TestExtend(Test):
+    def __init__(self, a_dict):
+        super().__init__(a_dict)
+        self._name = None
+
+    @Property(name="name2")
+    def name2(self, a_value):
         self._name = a_value
